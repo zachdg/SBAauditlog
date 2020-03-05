3a2273969f07df6f52f990bfd64af214a6afb6cd
zachdg <zachdg645@hotmail.com>
2020-03-05 11:15:39 -0800

    first push

---
+++ app.py
@@ -0,0 +1,54 @@
+import connexion
+from connexion import NoContent
+import requests
+import json
+from pykafka import KafkaClient
+from datetime import datetime
+import yaml
+
+with open('app_conf.yaml', 'r') as f:
+    app_config = yaml.safe_load(f.read())
+
+
+
+def get_request_history(requestNum):
+    client = KafkaClient(hosts="{}:{}".format(app_config['kafka']['server'], app_config['kafka']['port']))
+    topic = client.topics['events']
+    consumer = topic.get_simple_consumer(reset_offset_on_start=True, consumer_timeout_ms=100)
+
+    reqlist = []
+    for msg in consumer:
+        msg_str = msg.value.decode('utf-8')
+        msg = json.loads(msg_str)
+        if msg['type'] == 'request':
+            reqlist.append(msg)
+
+    if requestNum >= len(reqlist):
+        return NoContent, 404
+    return reqlist[requestNum], 200
+
+
+def get_report_oldest():
+    client = KafkaClient(hosts="{}:{}".format(app_config['kafka']['server'], app_config['kafka']['port']))
+    topic = client.topics['events']
+    consumer = topic.get_simple_consumer(reset_offset_on_start=True, consumer_timeout_ms=100)
+
+    replist = []
+    for msg in consumer:
+        msg_str = msg.value.decode('utf-8')
+        msg = json.loads(msg_str)
+        if msg['type'] == 'report':
+            replist.append(msg)
+
+    if len(replist) == 0 :
+        return NoContent, 404
+    else:
+        return replist[-1], 200
+
+
+app = connexion.FlaskApp(__name__, specification_dir='')
+app.add_api("openapi.yaml")
+
+if __name__ == "__main__":
+    app.run(port=8040)
+
